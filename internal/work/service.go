b015d277fdcb971dfd1dc7978397d168fbd5e428
oleg-ssvlabs <oleg.iar@ssvlabs.io>
2024-11-04 16:24:02 +0100

    Refactor metrics, update traces

--- internal/work/service.go
+++ internal/work/service.go
@@ -2,48 +2,60 @@ package work
 
 import (
 	"context"
+	"errors"
 	"log/slog"
 
 	"github.com/google/uuid"
 	"go.opentelemetry.io/otel"
 	"go.opentelemetry.io/otel/attribute"
 	"go.opentelemetry.io/otel/codes"
-	"go.opentelemetry.io/otel/metric"
 	"go.opentelemetry.io/otel/trace"
 )
 
-const name = "github.com/ssvlabsinfra/p2p-observability/internal/work"
+var tracer = otel.Tracer(name)
 
-var (
-	tracer  = otel.Tracer(name)
-	meter   = otel.Meter(name, metric.WithInstrumentationVersion("0.0.1"))
-	counter metric.Int64Counter
+type (
+	metrics interface {
+		Increment(ctx context.Context)
+	}
+	Work struct {
+		metrics metrics
+	}
 )
 
-func init() {
-	var err error
-	counter, err = meter.Int64Counter("p2p-observability", metric.WithDescription("p2p observability counter description"))
-	if err != nil {
-		panic(err)
+func New(metrics metrics) *Work {
+	return &Work{
+		metrics: metrics,
 	}
 }
 
-type Work struct {
-}
-
-func New() *Work {
-	return &Work{}
-}
-
-func (Work) Do(ctx context.Context, uuid uuid.UUID) {
+func (w *Work) Do(ctx context.Context, uuid uuid.UUID) {
 	ctx, span := tracer.Start(ctx, "work", trace.WithAttributes(attribute.String("id", uuid.String())))
 	defer span.End()
 
-	counter.Add(ctx, 1, metric.WithAttributes(attribute.String("attr", "custom_attribute")))
+	w.metrics.Increment(ctx)
 	span.AddEvent("work called", trace.WithAttributes(attribute.String("id", uuid.String())))
 	slog.
 		With("id", uuid.String()).
 		Info("work finished")
 
+	span.AddEvent("calling subwork")
+	if err := w.subWork(ctx, uuid); err != nil {
+		span.RecordError(err)
+		span.SetStatus(codes.Error, err.Error())
+		return
+	}
+
 	span.SetStatus(codes.Ok, "work finished")
+}
+
+func (Work) subWork(ctx context.Context, uuid uuid.UUID) error {
+	_, span := tracer.Start(ctx, "sub_work", trace.WithAttributes(attribute.String("id", uuid.String())))
+	defer span.End()
+
+	err := errors.New("work error")
+	span.RecordError(err)
+	span.SetStatus(codes.Error, err.Error())
+
+	return err
 }
